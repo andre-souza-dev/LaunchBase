ebe3d9b17028cb53c4ace5fdb7e1da53017cff09
AndreLuiz-JS <oxxxandreluizxxxo@gmail.com>
2020-07-02 13:02:41 -0300

    Desafio 3-1

---
+++ LaunchBase/Aulas/Desafio.3-1/src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import nunjucks from "nunjucks";
+
+const server = express();
+
+server.use(express.static("public"));
+server.set("view engine", "html");
+
+nunjucks.configure("src/views", {
+	express: server,
+});
+
+server.get("/", function (req, res) {
+	res.render("about.njk", { about: "selected" });
+});
+server.get("/courses", function (req, res) {
+	res.render("courses.njk", { courses: "selected" });
+});
+
+server.use(function (req, res) {
+	res.status(404).render("not-found.njk", { route: req.url });
+});
+
+server.listen("5000", function () {});
--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 
 ## Módulo 3 - Introdução ao Back-End
 
-- [ ] 1. Desafio 3.1 - Primeiro servidor
+- [x] 1. [Desafio 3.1 - Primeiro servidor](/LaunchBase/Aulas/Desafio.3-1)
 - [ ] 2. Desafio 3.2 - Nunjucks e dados dinâmicos
 - [ ] 3. Desafio 3.3 - Página de descrição de cursos
 - [ ] 4. Desafio 3.4 - Refatorando Foodfy (Adicionar a conexão com o backend)
